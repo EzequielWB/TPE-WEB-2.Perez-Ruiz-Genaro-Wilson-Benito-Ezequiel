5af4435f377e292694ddaff28030cd6a7bdc7576
Ezequiel <49461599+EzequielWB@users.noreply.github.com>
2024-10-04 13:09:43 -0300

    añadido indice, conexion con categorias, principio router

---
+++ conexion.php
@@ -0,0 +1,32 @@
+<?php
+
+$nombre_genero = $_GET['genre'];
+
+//conexion a base de datos 
+$db = new PDO('mysql:host=localhost;'.'dbname=catalogo_peliculas;charset=utf8', 'root', '');
+
+$query = $db->prepare('SELECT id, genero FROM generos WHERE genero = ?');
+$query->execute([$nombre_genero]);
+$genre = $query->fetch(PDO::FETCH_OBJ);
+
+// Consulta para obtener las películas relacionadas con el género
+$query = $db->prepare('SELECT nombre, director, descripcion FROM peliculas WHERE genero = ?');
+$query->execute([$genre->id]);
+$movies = $query->fetchAll(PDO::FETCH_OBJ);
+
+
+echo "<h1>Genero: $genre->genero</h1>";
+echo "
+    <ul>";
+        foreach ($movies as $movie) {
+            echo "<li>
+                    <h3>Nombre: $movie->nombre</h3>
+                    <p>Dirijida por: $movie->director</p>
+                    <p>Descripcion breve: $movie->descripcion</p>
+                </li>";
+    }
+echo"</ul>";
+
+?>
+
+<p><a href="../TPE/index.html">Volver al inicio</a></p>
