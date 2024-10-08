1848601972bbafdb070365e532b96f173bee4794
Ezequiel <EzequielWilsonBenito@gmail.com>
2024-10-08 16:39:48 -0300

    eliminado conexion.php y modificado el view

--- app/views/task.view.php
+++ app/views/task.view.php
@@ -6,8 +6,9 @@ class movieView {
         
         echo '<ul>';
         foreach ($movies as $movie) {
-           echo "<li>
-                    <span> <b>$movie->nombre</b> - $movie->director (prioridad $movie->descripcion) </span>
+           echo "<li style='border:1px solid black; margin-bottom:1px'>
+                    <span> <h1>$movie->nombre</h1> <h3>$movie->director</h3> </span>
+                        <p>$movie->descripcion</p>
                 </li>";
         }
         echo "</ul>";
