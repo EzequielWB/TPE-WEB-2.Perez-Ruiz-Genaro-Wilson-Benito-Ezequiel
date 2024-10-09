7504ef0cbde39edad92851f07f4b20fdf14570ff
Ezequiel <EzequielWilsonBenito@gmail.com>
2024-10-09 16:05:20 -0300

    cambios en MVC

--- app/controllers/task.controller.php
+++ app/controllers/task.controller.php
@@ -2,6 +2,7 @@
 require_once './app/models/task.model.php';
 require_once './app/views/task.view.php';
 
+
 class MovieController {
     private $model;
     private $view;
@@ -15,4 +16,26 @@ class MovieController {
         $peliculas = $this->model->getAllMovies();
         $this->view->showMovies($peliculas);
     }
+
+    // Nuevo método para mostrar películas por género
+    public function showMoviesByGenre($genre_id) {
+        $peliculas = $this->model->getMoviesByGenre($genre_id);
+        $this->view->showMovies($peliculas);
+    }
 }
+
+class GenreController {
+    private $model;
+    private $view;
+
+    public function __construct(){
+        $this->model = new GenresModel(); // El nombre correcto es GenresModel, no genresModel
+        $this->view = new GenresView(); // Cambiar a GenresView
+    }
+
+    public function showGenres() {
+        $genres = $this->model->getAllGenres();
+        $this->view->showGenres($genres);
+    }
+}
+?>
--- router.php
+++ router.php
@@ -3,20 +3,35 @@ require_once './app/controllers/task.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'movie';
+// Obtener acción desde la URL (por defecto 'genre')
+$action = 'genre';
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
 $params = explode('/',$action);
 
+// Inicializar controladores
 $movieController = new MovieController();
+$genreController = new GenreController();
 
 switch ($params[0]) {
+    case 'genre':
+        $genreController->showGenres();
+        break;
     case 'movie':
         $movieController->showMovies();
+        break;
+    case 'moviesByGenre':
+        if (isset($params[1])) { // Verificar si se pasó un ID de género
+            $genre_id = $params[1];
+            $movieController->showMoviesByGenre($genre_id);
+        } else {
+            echo('404 Page not Found');
+        }
         break;
     default:
         echo('404 Page not Found');
         break;
 }
+?>
