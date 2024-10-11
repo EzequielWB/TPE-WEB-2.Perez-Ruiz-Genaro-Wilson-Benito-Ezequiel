aaf209767a1658b6c3cedb98652a3e0789dca64f
Genaro Perez <62978480+GenaroPerez@users.noreply.github.com>
2024-10-11 01:02:34 -0300

    Actualizacion de router.php y hta

--- router.php
+++ router.php
@@ -1,37 +1,54 @@
 <?php
-require_once './app/controllers/task.controller.php';
+
+include_once 'app/controllers/movies.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
 // Obtener acción desde la URL (por defecto 'genre')
-$action = 'genre';
+$action = 'principal';
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
+} else {
+    $action = 'principal';
 }
 
 $params = explode('/',$action);
 
-// Inicializar controladores
-$movieController = new MovieController();
-$genreController = new GenreController();
+$controllerGenres = new GenreController(); //llamo al controller de otro archivo
+$controllerMovie = new MovieController();
+$controllerAddMovie = new addMovieController();
+$controllerDeleteMovie = new MovieController();
 
 switch ($params[0]) {
-    case 'genre':
-        $genreController->showGenres();
-        break;
-    case 'movie':
-        $movieController->showMovies();
-        break;
-    case 'moviesByGenre':
-        if (isset($params[1])) { // Verificar si se pasó un ID de género
-            $genre_id = $params[1];
-            $movieController->showMoviesByGenre($genre_id);
+
+    case 'principal':
+        $controllerGenres->showGenres();
+    break;
+
+    case 'peliculas':
+        if (isset($params[1])) {
+            $genreId = $params[1];  
+            $controllerMovie->showMoviesByGenre($genreId); 
         } else {
-            echo('404 Page not Found');
+            $controllerGenres->showGenres();
         }
-        break;
-    default:
-        echo('404 Page not Found');
-        break;
+    break;
+
+    case 'detalle':
+            $movieId = $params[1]; // id desde url
+            $controllerMovie->showMovieDetails($movieId);
+    break;
+
+    case 'añadir':
+        $controllerAddMovie->addMovieGenres();  
+    break;
+
+    case 'guardar': 
+        $controllerAddMovie->addMovieControl(); 
+    break;
+
+    case 'borrar':
+        $controllerDeleteMovie->deleteMovie($params[1]);
+    break;
 }
 ?>
